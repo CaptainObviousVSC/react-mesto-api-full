740ecfa74db33e1fa8974969e7ce737ea8e79b8c
Aziik <arslanfazilov93@gmail.com>
2020-12-08 22:08:29 +0300

    исправлено

--- backend/app.js
+++ backend/app.js
@@ -33,17 +33,15 @@ app.use('/', authFunction, usersRoutes);
 app.use('/', authFunction, cardsRoutes);
 app.use(errorLogger);
 app.use(errors());
-app.use((err, req, res, next) => {
-  res.status(err.statusCode).send({ message: err.message });
-  next();
-});
-app.use((err, req, res) => {
-  const { statusCode = 500, message } = err;
-
-  res.status(statusCode).send({
-    message: statusCode === 500
-      ? 'На сервере произошла ошибка'
-      : message,
-  });
-});
+app.use((err, req, res, next) => { 
+  const { statusCode = 500, message } = err; 
+ 
+  res 
+    .status(statusCode) 
+    .send({ 
+      message: statusCode === 500 
+        ? 'На сервере произошла ошибка' 
+        : message 
+    }); 
+});   
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
